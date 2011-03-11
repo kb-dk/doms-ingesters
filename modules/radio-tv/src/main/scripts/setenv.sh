86708155fb26c00dfec78e85b151d0c162466d46
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2011-03-11 11:24:24 +0000

    Should bundle correctly now

--- modules/radio-tv/src/main/scripts/setenv.sh
+++ modules/radio-tv/src/main/scripts/setenv.sh
@@ -6,8 +6,8 @@ COLDFOLDER=$BASEDIR/files/coldfolder
 LUKEFOLDER=$BASEDIR/files/lukewarm
 HOTFOLDER=$BASEDIR/files/hotfolder
 STOPFOLDER=$BASEDIR/files/stopfolder
-WSDL=http://alhena:7980/centralDomsWebservice/central/?wsdl
+WSDL=http://localhost:7980/centralWebservice-service/central/?wsdl
 USERNAME=fedoraAdmin
 PASSWORD=fedoraAdminSecretPass
-SCHEMA=$BASEDIR/config/preingestedRadioTVProgram.xsd
+SCHEMA=$BASEDIR/resources/preingestedRadioTVProgram.xsd
 
