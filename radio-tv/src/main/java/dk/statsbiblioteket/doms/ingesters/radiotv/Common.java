82248bde706f253357c895080e8ff6e6468134ae
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2016-09-01 13:45:07 +0200

    Better order in common

--- radio-tv/src/main/java/dk/statsbiblioteket/doms/ingesters/radiotv/Common.java
+++ radio-tv/src/main/java/dk/statsbiblioteket/doms/ingesters/radiotv/Common.java
@@ -5,6 +5,7 @@ import dk.statsbiblioteket.util.xml.XPathSelector;
 
 /** Constants used while building document. */
 public class Common {
+
     public static final String COMMENT = "Ingest of Radio/TV data";
     public static final String FAILED_COMMENT = COMMENT + ": Something failed, rolling back";
     public static final int MAX_FAIL_COUNT = 10;
@@ -20,9 +21,9 @@ public class Common {
                                  "ritzau", RITZAU_NAMESPACE,
                                  "gallup", GALLUP_NAMESPACE,
                                  "pb", PROGRAM_BROADCAST_NAMESPACE);
+
     public static final String PBCORE_DESCRIPTION_ELEMENT
                     = "//program/pbcore/pbc:PBCoreDescriptionDocument";
-    public static final String DC_IDENTIFIER_ELEMENT = "//dc:identifier";
     public static final String PBCORE_TITLE_ELEMENT = "//pbc:pbcoreTitle[pbc:titleType=\"titel\"]/pbc:title";
     public static final String PBCORE_RITZAU_IDENTIFIER_ELEMENT
             = "pbc:pbcoreIdentifier[pbc:identifierSource=\"id\"]/pbc:identifier";
@@ -31,6 +32,9 @@ public class Common {
     public static final String RITZAU_ORIGINALS_ELEMENT = "//program/originals/ritzau:ritzau_original";
     public static final String GALLUP_ORIGINALS_ELEMENT = "//program/originals/gallup:gallup_original|//program/originals/gallup:tvmeterProgram";
     public static final String PROGRAM_BROADCAST_ELEMENT = "//program/pb:programBroadcast";
+
+    public static final String DC_IDENTIFIER_ELEMENT = "//dc:identifier";
+
     public static final String RECORDING_FILES_URLS = "//program/fileUrls/fileUrl";
 
     public static final String PROGRAM_TEMPLATE_PID = "doms:Template_Program";
