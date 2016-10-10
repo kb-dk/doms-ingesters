56cb7983cf92a92b01e5369405a137165b99834d
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2016-10-10 10:33:22 +0200

    Review followup. Some tests and whitespaces

--- radio-tv/src/test/java/dk/statsbiblioteket/doms/ingesters/radiotv/UtilTest.java
+++ radio-tv/src/test/java/dk/statsbiblioteket/doms/ingesters/radiotv/UtilTest.java
@@ -20,4 +20,55 @@ public class UtilTest {
         String comment = Util.domsCommenter("testfileName", "test action with arg '{0}'", "testArg");
         assertEquals("RadioTV Digitv Ingester (null) test action with arg 'testArg' as part of ingest of testfileName",comment);
     }
+
+
+    @Test
+    public void doms2Comments() throws Exception {
+        String comment = Util.domsCommenter("testfileName", "test action with arg {0} and {1}", "testArg", " testarg2 ");
+        assertEquals("RadioTV Digitv Ingester (null) test action with arg testArg and  testarg2  as part of ingest of testfileName",comment);
+    }
+
+    @Test
+    public void weirdFilename() throws Exception {
+        String comment = Util.domsCommenter("testfileName{0}", "test action with arg {0}", "testArg");
+        assertEquals("RadioTV Digitv Ingester (null) test action with arg testArg as part of ingest of testfileName{0}",comment);
+    }
+
+    @Test
+    public void repeatedComments() throws Exception {
+        String comment = Util.domsCommenter("testfileName", "test action with arg {0} and {0}", "testArg");
+        assertEquals("RadioTV Digitv Ingester (null) test action with arg testArg and testArg as part of ingest of testfileName",comment);
+    }
+
+    @Test
+    public void unusedComments() throws Exception {
+        String comment = Util.domsCommenter("testfileName", "test action with arg {0}", "testArg", " testArg2 ");
+        assertEquals("RadioTV Digitv Ingester (null) test action with arg testArg as part of ingest of testfileName",comment);
+    }
+
+    @Test
+    public void indexOutOfBounds() throws Exception {
+        String comment = Util.domsCommenter("testfileName", "test action with arg {5}", "testArg");
+        assertEquals("RadioTV Digitv Ingester (null) test action with arg {5} as part of ingest of testfileName",comment);
+    }
+
+    @Test
+    public void nestedArgs() throws Exception {
+        String comment = Util.domsCommenter("testfileName", "test action with arg {0}", "testArg{0}");
+        assertEquals("RadioTV Digitv Ingester (null) test action with arg testArg{0} as part of ingest of testfileName",comment);
+    }
+
+    @Test
+    public void nestedArgs2() throws Exception {
+        String comment = Util.domsCommenter("testfileName", "test action with arg {0}", "testArg{1}", "arg2");
+        assertEquals("RadioTV Digitv Ingester (null) test action with arg testArg{1} as part of ingest of testfileName",comment);
+    }
+
+    @Test
+    public void newLines() throws Exception {
+        String comment = Util.domsCommenter("testfile\nName\n", "test action with \narg {0}", "\ntestArg\n");
+        assertEquals("RadioTV Digitv Ingester (null) test action with \narg \ntestArg\n as part of ingest of testfile\nName\n",comment);
+    }
+
+
 }
